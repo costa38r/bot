f596b66fa6cf610380f9e138666b3367095776a6
costa38r <costa38r@proton.me>
2024-12-19 23:07:30 -0300

    algumas correcoes nos comentarios

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,4 @@
+// The main package initializes and starts the WhatsApp client.
 package main
 
 import (
@@ -7,10 +8,10 @@ import (
 )
 
 func main() {
-
+	// RunClient handles the entire lifecycle of the WhatsApp client.
+	// If an error occurs, it will be logged to the console.
 	err := whatsapp.RunClient()
 	if err != nil {
 		fmt.Println("error running client: ", err)
-
 	}
 }
