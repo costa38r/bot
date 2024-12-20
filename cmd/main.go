78fa3f855ec90b675f53f920fac328c5b0f7a0b7
costa38r <costa38r@proton.me>
2024-12-19 22:25:59 -0300

    Client criado, conectando e recebendo eventos

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/costa38r/bot/pkg/whatsapp"
+)
+
+func main() {
+	container, err := whatsapp.ConfigContainer()
+	if err != nil {
+		fmt.Println("Error configuring container:", err)
+		return
+	}
+
+	client, err := whatsapp.ConfigClient(container)
+	if err != nil {
+		fmt.Println("Error configuring client:", err)
+		return
+	}
+
+	err = client.RunClient()
+	if err != nil {
+		fmt.Println("Error running client:", err)
+	}
 }
